36a6d6bc000b06af837eeaa3ce0adadee613fdc1
Deliciaes <ceramstedt@gmail.com>
2021-10-24 20:13:43 +0200

    re-upload catsdata.php
    accidentally deleted this file...

---
+++ catsdata.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+
+$cats = [
+
+    ['name' => 'Charlie', 'age' => 8, 'gender' => 'male', 'pronoun' => 'his', 'fur color' => 'multicolored', 'eye color' => 'blue', 'photo' => 'photos/charlie.png'],
+    ['name' => 'Busen', 'age' => 4, 'gender' => 'male', 'pronoun' => 'his', 'fur color' => 'multicolored', 'eye color' => 'yellow', 'photo' => 'photos/busen.png'],
+    ['name' => 'Pip', 'age' => 4, 'gender' => 'female', 'pronoun' => 'her', 'fur color' => 'black', 'eye color' => 'yellow', 'photo' => 'photos/pip.png'],
+    ['name' => 'Nisse', 'age' => 4, 'gender' => 'male', 'pronoun' => 'his', 'fur color' => 'multicolored', 'eye color' => 'green', 'photo' => 'photos/nisse.png'],
+    ['name' => 'Kylo', 'age' => 2, 'gender' => 'male', 'pronoun' => 'his', 'fur color' => 'black', 'eye color' => 'yellow', 'photo' => 'photos/kylo.png'],
+    ['name' => 'Ginny', 'age' => 11, 'gender' => 'female', 'pronoun' => 'her', 'fur color' => 'multicolored', 'eye color' => 'green', 'photo' => 'photos/ginny.png'],
+    ['name' => 'Pippa', 'age' => 11, 'gender' => 'female', 'pronoun' => 'her', 'fur color' => 'muluticolored', 'eye color' => 'yellow', 'photo' => 'photos/pippa.png'],
+    ['name' => 'Mr. Fluff Fluff', 'age' => 7, 'gender' => 'female', 'pronoun' => 'her', 'fur color' => 'tabby', 'eye color' => 'green', 'photo' => 'photos/mrflufffluff.png'],
+    ['name' => 'Blackie', 'age' => 6, 'gender' => 'male', 'pronoun' => 'his', 'fur color' => 'brown', 'eye color' => 'green', 'photo' => 'photos/blackie.png'],
+];
